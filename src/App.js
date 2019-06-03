9938686927cb32b370a799f5b4abd6d25df59ef2
Catalina Campos <catalina.camposcaceres9@gmail.com>
2019-06-03 02:03:05 -0400

    Página inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import marvel from './images/marvel.png';
+import carrusel from './images/carrusel.jpg';
+import button from './images/button.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <header className="principalHeader"><img src={marvel} alt="logo" className="logo" /></header>
+          <navbar className="navButtons"><button>CHARACTERS</button><button>LET'S PLAY</button></navbar>
+        </section>
+        
+        <img src={carrusel} alt="logo" className="carrusel" />
+        <div className="navUnderCarrusel"></div>
+        <img src={button} alt="logo" className="characters" />
+      </div>
+    )
+  }
+};
 
 export default App;
---
+++ src/components/Superheroes/Superheroes.js
@@ -0,0 +1,20 @@
+import React, { Component } from React;
+
+class Superheroes extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            superheroes = []
+        }
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
