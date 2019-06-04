2ff08a017add17df024761f010a7911a066d3cd5
Catalina Campos <catalina.camposcaceres9@gmail.com>
2019-06-04 17:08:24 -0400

    Cambio de fetch hacia app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,43 @@ import carrusel from './images/carrusel.jpg';
 import button from './images/button.png';
 
 import Superheroes from './components/Superheroes/Superheroes'
+//import SuperheroesInfo from './components/SuperheroesInfo/SuperheroesInfo'
+
+const API_KEY = '139853e75bfe04a8b492968608147a1a';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      superheroes: []
+    }
+  }
+
+  /*superheroeSearch = async (e) => {
+    const superheroeName = e.target.elements.superheroeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${API_KEY}&hash=07d6647c6490ca8893a4ef6d7a666c9b&limit=100`);
+
+    const data = await api_call.json()
+    this.setState({
+      superheroes: data.data.results
+    });
+    console.log(this.state.superheroes)
+  }*/
+
+  componentDidMount() {
+    fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=139853e75bfe04a8b492968608147a1a&hash=07d6647c6490ca8893a4ef6d7a666c9b&limit=100")
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                superheroes: json.data.results
+            })
+            console.log(this.state.superheroes)
+        });
+}
+
   render() {
+
     return (
       <div>
         <section>
@@ -22,9 +56,8 @@ class App extends Component {
 
         <img src={button} alt="logo" className="characters" />
 
-        
+        <Superheroes superheroes={this.state.superheroes}/>
 
-        <Superheroes />
       </div>
     )
   }
--- src/components/Superheroes/Superheroes.js
+++ src/components/Superheroes/Superheroes.js
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Superheroes.css'
 
-class Superheroes extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            superheroes: []
-        }
-    }
-
-    componentDidMount() {
-        fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=139853e75bfe04a8b492968608147a1a&hash=07d6647c6490ca8893a4ef6d7a666c9b&limit=100")
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    superheroes: json.data.results
-                })
-                console.log(this.state.superheroes)
-            });
-    }
-
-    render() {
-        const img = this.state.superheroes.map(item => {
-            return <div className="img"> <img  className="imgAlone" src={item.thumbnail.path + '/portrait_fantastic.' + item.thumbnail.extension} alt="" key={item.id} />
-                <div className="superheroeName">{item.name}</div>
-            </div>
-        });
-
-        return (
-            <div className="charactersDiv"> 
-            <section className="filterOrderSection">
-                <input className="searchInput" placeholder="SEARCH"></input>
-                <select className="selectOrder"></select>
-            </section>
-
-            <section className="superheroesImg">
-                {img}
-            </section>
-            </div>
-        )
-    }
-}
+const Superheroes = props => (
+    <section className="superheroesImg">
+        {props.superheroes.map((item) => {
+            return (
+                <div className="charactersDiv">
+                    <div className="img">
+                        <img className="imgAlone" src={item.thumbnail.path + '/portrait_fantastic.' + item.thumbnail.extension} alt="" key={item.id} />
+                        <div className="superheroeName">{item.name}</div>
+                    </div>
+                </div>
+            )
+        })}
+    </section>
+)
 
 export default Superheroes;
+
+/*componentDidMount() {
+    fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=139853e75bfe04a8b492968608147a1a&hash=07d6647c6490ca8893a4ef6d7a666c9b&limit=100")
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                superheroes: json.data.results
+            })
+            console.log(this.state.superheroes)
+        });
+}
+
+return (
+
+
+        <section className="superheroesImg">
+            {img}
+        </section>
+    </div>
+)
+    }
+)
+*/
